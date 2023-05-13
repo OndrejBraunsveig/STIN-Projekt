4b59602df00ed0561d3a3a25e98aa5faec0f0cf0
unknown <ondrej.braunsveig@tul.cz>
2023-05-13 20:39:38 +0200

    Parse function for txt to json

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
+import datetime
+import re
 import random
 import string
 import smtplib
+import json
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
@@ -28,6 +31,7 @@ def login():
                 return redirect(url_for('account', username=email))
             return render_template('index.html', message='Wrong verification code!')
     
+    parse_rates()
     # Remove verification code validity on webserver start
     with open('code.txt', 'w') as file:
         file.write("")
@@ -59,6 +63,35 @@ def send_mail(code):
         server.login(email, password) # Login with email and password
         text = msg.as_string()
         server.sendmail(email, email, text)
+
+def parse_rates():
+    with open('data/denni_kurz.txt', 'r') as file:
+        file.readline()
+        file.readline()
+        lines = file.readlines()
+    today = datetime.datetime.now()
+    dictionary = {
+        "hour": today.hour,
+        "day": today.day,
+        "month": today.month,
+        "year": today.year
+    }
+    for line in lines:
+        line = line.replace(',', '.')
+        line = line.strip()
+        split_line = line.split('|')
+        dictionary[split_line[3]] = float(split_line[4])/float(split_line[2])
+    with open('data/exchange_rates.json', 'w') as file:
+        json.dump(dictionary, file)
+
+
+
+def check_rates():
+    with open('data/denni_kurz.txt', 'r') as file:
+        line = file.readline()
+    date = list(map(int, re.split(r'[. ]', line)[:3]))
+    today = datetime.datetime.now()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
